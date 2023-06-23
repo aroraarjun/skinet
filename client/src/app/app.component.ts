aba20be5a2105b21b0566a0f8ebe488b8f355639
Arjun Arora <arjunarora171997@gmail.com>
2023-06-23 07:31:19 +0530

    Response Model and Nav Bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
+import { Product } from './Models/product';
+import { Pagination } from './Models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('http://localhost:5114/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data, //what to do next when req completed
+      error: error=> console.log(error), // what to when error
+      complete: () => {
+        console.log('req completed');
+        console.log('extra statement');
+      }
+      //automatically unsubscribes
+    })
+  }
 }
